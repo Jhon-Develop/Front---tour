8f2fac61876fa43a75ec7b7372ef07ffa6cf7105
David Osorio <daalosx@gmail.com>
2024-05-16 15:40:42 -0500

    change_16_mayo_webpack_dashHome_footer

---
+++ bundle/bundle.js
@@ -0,0 +1,32 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./scripts/index.js":
+/*!**************************!*\
+  !*** ./scripts/index.js ***!
+  \**************************/
+/***/ (() => {
+
+eval("// Start francisco\n\n// Finish francisco\n// Start alejandro\n\n// Finish alejandro\n// Start juan_pablo\n\n// Finish juan_pablo\n// Start samuel\n\n// Finish samuel\n\n//# sourceURL=webpack://tour-hopper-project/./scripts/index.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./scripts/index.js"]();
+/******/ 	
+/******/ })()
+;
