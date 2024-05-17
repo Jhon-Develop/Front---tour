129f6d1422133b4424259329d06f0becd604fd7a
samuelacevedo9307 <samuelacevedo9307@gmail.com>
2024-05-16 21:27:57 -0500

    change_16_mayo_dashhome_footer

--- bundle/bundle.js
+++ bundle/bundle.js
@@ -15,7 +15,7 @@
   \**************************/
 /***/ (() => {
 
-eval("// Start francisco\n\n// Finish francisco\n// Start alejandro\n\n// Finish alejandro\n// Start juan_pablo\n\n// Finish juan_pablo\n// Start samuel\n\n// Finish samuel\n\n//# sourceURL=webpack://tour-hopper-project/./scripts/index.js?");
+eval("throw new Error(\"Module build failed (from ./node_modules/babel-loader/lib/index.js):\\nError: Cannot find module 'fs/promises'\\nRequire stack:\\n- /home/riwip4o-025/Desktop/samuel/Front---tour/node_modules/babel-loader/lib/cache.js\\n- /home/riwip4o-025/Desktop/samuel/Front---tour/node_modules/babel-loader/lib/index.js\\n- /home/riwip4o-025/Desktop/samuel/Front---tour/node_modules/loader-runner/lib/loadLoader.js\\n- /home/riwip4o-025/Desktop/samuel/Front---tour/node_modules/loader-runner/lib/LoaderRunner.js\\n- /home/riwip4o-025/Desktop/samuel/Front---tour/node_modules/webpack/lib/NormalModuleFactory.js\\n- /home/riwip4o-025/Desktop/samuel/Front---tour/node_modules/webpack/lib/Compiler.js\\n- /home/riwip4o-025/Desktop/samuel/Front---tour/node_modules/webpack/lib/webpack.js\\n- /home/riwip4o-025/Desktop/samuel/Front---tour/node_modules/webpack/lib/index.js\\n- /home/riwip4o-025/Desktop/samuel/Front---tour/node_modules/webpack-cli/lib/webpack-cli.js\\n- /home/riwip4o-025/Desktop/samuel/Front---tour/node_modules/webpack-cli/lib/bootstrap.js\\n- /home/riwip4o-025/Desktop/samuel/Front---tour/node_modules/webpack-cli/bin/cli.js\\n- /home/riwip4o-025/Desktop/samuel/Front---tour/node_modules/webpack/bin/webpack.js\\n    at Function.Module._resolveFilename (internal/modules/cjs/loader.js:815:15)\\n    at Function.Module._load (internal/modules/cjs/loader.js:667:27)\\n    at Module.require (internal/modules/cjs/loader.js:887:19)\\n    at require (internal/modules/cjs/helpers.js:74:18)\\n    at Object.<anonymous> (/home/riwip4o-025/Desktop/samuel/Front---tour/node_modules/babel-loader/lib/cache.js:21:5)\\n    at Module._compile (internal/modules/cjs/loader.js:999:30)\\n    at Object.Module._extensions..js (internal/modules/cjs/loader.js:1027:10)\\n    at Module.load (internal/modules/cjs/loader.js:863:32)\\n    at Function.Module._load (internal/modules/cjs/loader.js:708:14)\\n    at Module.require (internal/modules/cjs/loader.js:887:19)\");\n\n//# sourceURL=webpack://tour-hopper-project/./scripts/index.js?");
 
 /***/ })
 
@@ -24,7 +24,7 @@ eval("// Start francisco\n\n// Finish francisco\n// Start alejandro\n\n// Finish
 /******/ 	
 /******/ 	// startup
 /******/ 	// Load entry module and return exports
-/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	// This entry module doesn't tell about it's top-level declarations so it can't be inlined
 /******/ 	var __webpack_exports__ = {};
 /******/ 	__webpack_modules__["./scripts/index.js"]();
 /******/ 	
