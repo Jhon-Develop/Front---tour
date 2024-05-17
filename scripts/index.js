867d1c1479e37727e1024eaa6e69049a79e3381c
franciscoaguirredev <franco.hopper.riwi@gmail.com>
2024-05-16 19:13:29 -0500

    no hacer pull request aun

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,21 @@
 // Start francisco
+const $menuButton = document.querySelector(".menu-toggle");
+const $menu = document.getElementById("nav-menu");
+console.log({ $menu });
+const ARIA_EXPANDED = "aria-expanded";
 
+$menuButton.addEventListener("click", () => {
+  const isMenuOpened = $menuButton.getAttribute(ARIA_EXPANDED) === "true"; // -> 'true' 'false'
+  isMenuOpened ? closeMenu($menu) : openMenu($menu);
+});
+
+function openMenu() {
+  $menuButton.setAttribute(ARIA_EXPANDED, "true");
+}
+
+function closeMenu() {
+  $menuButton.setAttribute(ARIA_EXPANDED, "false");
+}
 // Finish francisco
 // Start alejandro
 
