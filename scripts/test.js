d8accc3a0a049a58c5fe205b468ddb69afc9d016
Juan Agudelo <agudelojuanpablo398@gmail.com>
2024-05-26 13:30:30 -0500

    have been add button see more

--- scripts/test.js
+++ scripts/test.js
@@ -9,52 +9,6 @@ const ApiTours = [
     description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minima velit facilis iste dolores, quae molestias corporis recusandae culpa, ad obcaecati repellat molestiae, mollitia nihil expedita odit! Deserunt, eum. Officia, quae."
   },
 
-  {
-    image: 'https://images.adsttc.com/media/images/5a4b/c9b5/b22e/38b5/fa00/00ac/newsletter/Screen_Shot_2018-01-02_at_12.05.08_PM.jpg?1514916273',
-    name: "Visit the Sagrada Familia San Nicolas",
-    price: "78.9 USD",
-    raiting: 5,
-    numOpinions: "1,000 opiniones acerca de este tour",
-    description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minima velit facilis iste dolores, quae molestias corporis recusandae culpa, ad obcaecati repellat molestiae, mollitia nihil expedita odit! Deserunt, eum. Officia, quae."
-  },
-
-  {
-    image: 'https://images.adsttc.com/media/images/5a4b/c9b5/b22e/38b5/fa00/00ac/newsletter/Screen_Shot_2018-01-02_at_12.05.08_PM.jpg?1514916273',
-    name: "Visit the Sagrada Familia San Nicolas",
-    price: "78.9 USD",
-    raiting: 5,
-    numOpinions: "1,000 opiniones acerca de este tour",
-    description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minima velit facilis iste dolores, quae molestias corporis recusandae culpa, ad obcaecati repellat molestiae, mollitia nihil expedita odit! Deserunt, eum. Officia, quae."
-  },
-
-  {
-    image: 'https://images.adsttc.com/media/images/5a4b/c9b5/b22e/38b5/fa00/00ac/newsletter/Screen_Shot_2018-01-02_at_12.05.08_PM.jpg?1514916273',
-    name: "Visit the Sagrada Familia San Nicolas",
-    price: "78.9 USD",
-    raiting: 5,
-    numOpinions: "1,000 opiniones acerca de este tour",
-    description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minima velit facilis iste dolores, quae molestias corporis recusandae culpa, ad obcaecati repellat molestiae, mollitia nihil expedita odit! Deserunt, eum. Officia, quae."
-  },
-
-
-  {
-    image: 'https://images.adsttc.com/media/images/5a4b/c9b5/b22e/38b5/fa00/00ac/newsletter/Screen_Shot_2018-01-02_at_12.05.08_PM.jpg?1514916273',
-    name: "Visit the Sagrada Familia San Nicolas",
-    price: "78.9 USD",
-    raiting: 5,
-    numOpinions: "1,000 opiniones acerca de este tour",
-    description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minima velit facilis iste dolores, quae molestias corporis recusandae culpa, ad obcaecati repellat molestiae, mollitia nihil expedita odit! Deserunt, eum. Officia, quae."
-  },
-
-  {
-    image: 'https://images.adsttc.com/media/images/5a4b/c9b5/b22e/38b5/fa00/00ac/newsletter/Screen_Shot_2018-01-02_at_12.05.08_PM.jpg?1514916273',
-    name: "Visit the Sagrada Familia San Nicolas",
-    price: "78.9 USD",
-    raiting: 5,
-    numOpinions: "1,000 opiniones acerca de este tour",
-    description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minima velit facilis iste dolores, quae molestias corporis recusandae culpa, ad obcaecati repellat molestiae, mollitia nihil expedita odit! Deserunt, eum. Officia, quae."
-  },
-
     {
     image: 'https://images.adsttc.com/media/images/5a4b/c9b5/b22e/38b5/fa00/00ac/newsletter/Screen_Shot_2018-01-02_at_12.05.08_PM.jpg?1514916273',
     name: "Visit the Sagrada Familia San Nicolas",
@@ -83,7 +37,128 @@ const ApiTours = [
   },
 
 
+
+  {
+    image: 'https://images.adsttc.com/media/images/5a4b/c9b5/b22e/38b5/fa00/00ac/newsletter/Screen_Shot_2018-01-02_at_12.05.08_PM.jpg?1514916273',
+    name: "Visit the Sagrada Familia San Nicolas",
+    price: "78.9 USD",
+    raiting: 5,
+    numOpinions: "1,000 opiniones acerca de este tour",
+    description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minima velit facilis iste dolores, quae molestias corporis recusandae culpa, ad obcaecati repellat molestiae, mollitia nihil expedita odit! Deserunt, eum. Officia, quae."
+  },
+
+  
+  {
+    image: 'https://images.adsttc.com/media/images/5a4b/c9b5/b22e/38b5/fa00/00ac/newsletter/Screen_Shot_2018-01-02_at_12.05.08_PM.jpg?1514916273',
+    name: "Visit the Sagrada Familia San Nicolas",
+    price: "78.9 USD",
+    raiting: 5,
+    numOpinions: "1,000 opiniones acerca de este tour",
+    description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minima velit facilis iste dolores, quae molestias corporis recusandae culpa, ad obcaecati repellat molestiae, mollitia nihil expedita odit! Deserunt, eum. Officia, quae."
+  },
+
+  
+  {
+    image: 'https://images.adsttc.com/media/images/5a4b/c9b5/b22e/38b5/fa00/00ac/newsletter/Screen_Shot_2018-01-02_at_12.05.08_PM.jpg?1514916273',
+    name: "Visit the Sagrada Familia San Nicolas",
+    price: "78.9 USD",
+    raiting: 5,
+    numOpinions: "1,000 opiniones acerca de este tour",
+    description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minima velit facilis iste dolores, quae molestias corporis recusandae culpa, ad obcaecati repellat molestiae, mollitia nihil expedita odit! Deserunt, eum. Officia, quae."
+  },
+
+
+  {
+    image: 'https://images.adsttc.com/media/images/5a4b/c9b5/b22e/38b5/fa00/00ac/newsletter/Screen_Shot_2018-01-02_at_12.05.08_PM.jpg?1514916273',
+    name: "Visit the Sagrada Familia San Nicolas",
+    price: "78.9 USD",
+    raiting: 5,
+    numOpinions: "1,000 opiniones acerca de este tour",
+    description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minima velit facilis iste dolores, quae molestias corporis recusandae culpa, ad obcaecati repellat molestiae, mollitia nihil expedita odit! Deserunt, eum. Officia, quae."
+  },
+
+
+  {
+    image: 'https://images.adsttc.com/media/images/5a4b/c9b5/b22e/38b5/fa00/00ac/newsletter/Screen_Shot_2018-01-02_at_12.05.08_PM.jpg?1514916273',
+    name: "Visit the Sagrada Familia San Nicolas",
+    price: "78.9 USD",
+    raiting: 5,
+    numOpinions: "1,000 opiniones acerca de este tour",
+    description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minima velit facilis iste dolores, quae molestias corporis recusandae culpa, ad obcaecati repellat molestiae, mollitia nihil expedita odit! Deserunt, eum. Officia, quae."
+  },
+
+  
+  {
+    image: 'https://images.adsttc.com/media/images/5a4b/c9b5/b22e/38b5/fa00/00ac/newsletter/Screen_Shot_2018-01-02_at_12.05.08_PM.jpg?1514916273',
+    name: "Visit the Sagrada Familia San Nicolas",
+    price: "78.9 USD",
+    raiting: 5,
+    numOpinions: "1,000 opiniones acerca de este tour",
+    description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minima velit facilis iste dolores, quae molestias corporis recusandae culpa, ad obcaecati repellat molestiae, mollitia nihil expedita odit! Deserunt, eum. Officia, quae."
+  },
+
+
+  {
+    image: 'https://images.adsttc.com/media/images/5a4b/c9b5/b22e/38b5/fa00/00ac/newsletter/Screen_Shot_2018-01-02_at_12.05.08_PM.jpg?1514916273',
+    name: "Visit the Sagrada Familia San Nicolas",
+    price: "78.9 USD",
+    raiting: 5,
+    numOpinions: "1,000 opiniones acerca de este tour",
+    description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minima velit facilis iste dolores, quae molestias corporis recusandae culpa, ad obcaecati repellat molestiae, mollitia nihil expedita odit! Deserunt, eum. Officia, quae."
+  },
+
+
+
+  {
+    image: 'https://images.adsttc.com/media/images/5a4b/c9b5/b22e/38b5/fa00/00ac/newsletter/Screen_Shot_2018-01-02_at_12.05.08_PM.jpg?1514916273',
+    name: "Visit the Sagrada Familia San Nicolas",
+    price: "78.9 USD",
+    raiting: 5,
+    numOpinions: "1,000 opiniones acerca de este tour",
+    description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minima velit facilis iste dolores, quae molestias corporis recusandae culpa, ad obcaecati repellat molestiae, mollitia nihil expedita odit! Deserunt, eum. Officia, quae."
+  },
+
 ];
+
+let cardsDisplayed = false
+function createCard(tour) {
+  const fragment = document.createDocumentFragment();
+  let $card = document.createElement('article');
+  $card.classList.add('cards_highlights');
+  $card.innerHTML = `
+    <div class="highlights_images">
+        <img src="${tour.image}" alt="${tour.name}" />
+    </div>
+    <div class="about_travel">
+        <h3 class="travel_name">${tour.name}</h3>
+        <button class="button_price">
+            <p>$${tour.price} USD</p>
+        </button>
+    </div>
+    <div class="travel_opinions">
+        <h2>${'<i class="fa-solid fa-star"></i>'.repeat(tour.raiting)}</h2>
+    </div>
+    <div class="num_opinions">
+        <p>${tour.numOpinions} plazas disponibles</p>
+    </div>
+    <!-- Más información -->
+    <div class="more_information">
+        <div class="more_information_text">
+            <h2>${'<i class="fa-solid fa-star"></i>'.repeat(tour.raiting)}</h2>
+            <p>${tour.description}</p>
+            <button class="travel_price_back">
+                <p>$${tour.price} USD</p>
+            </button>
+        </div>
+        <div class="contain_add_booking">
+            <h3>${tour.name}</h3>
+            <button>RESERVAR</button>
+        </div>
+    </div>
+  `;
+  fragment.appendChild($card);
+  return fragment;
+}
 
 function createCard(tour) {
   const fragment = document.createDocumentFragment();
@@ -125,8 +200,20 @@ function createCard(tour) {
 }
 
 
-const containMain = document.querySelector(".P_section_highlights");
-ApiTours.forEach((card) => {
-  const carta = createCard(card);
-  containMain.appendChild(carta);
-});
+
+function showAllCards() {
+  if (!cardsDisplayed) {
+    const container = document.querySelector('.P_section_highlights');
+    ApiTours.forEach(card => {
+      const cardElement = createCard(card);
+      container.appendChild(cardElement);
+    });
+    document.querySelectorAll('.cards_highlights').forEach(card => {
+      card.style.display = 'block';
+    });
+    cardsDisplayed = true; // Marcar que las cartas ya se han mostrado
+    document.querySelector('.see_more').disabled = true; // Deshabilitar el botón "Ver más"
+  }
+}
+
+document.querySelector('.see_more').addEventListener('click', showAllCards);
